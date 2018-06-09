c40d2ec68d0a99313f176efe7fc6fa13e941e4f7
qrsl <david.qarseladze@gmail.com>
2018-06-09 16:56:11 +0400

    sada

---
+++ 9_june_2018/js/app.js
@@ -0,0 +1,82 @@
+"use strict"
+const COLOR_GREEN = "#0f0";
+const COLOR_ORANGE = "#ff7f00" ;
+// debugger
+//  alert("Hello World");  
+// confirm("are you okay ?");
+// prompt("say something");
+
+//ASI
+// alert("1")
+// alert("2")
+/* 
+multiline comment
+*/
+//var, let, const
+// let message;
+// message = "my message";
+// console.log(message);
+
+// let user = "John", age = 33;
+// console.log(message, user, age);
+// camelCase
+
+//  num = 13;
+//  console.log(num);
+
+// let num = 15;
+// {
+//       num = 17;
+//     //    console.log(num);
+// }
+           
+// console.log(num);
+
+
+
+// let foo = 99;
+// console.log(typeof foo );
+// foo = "bar";
+// console.log(typeof foo );
+// foo = true;
+// console.log(typeof foo);
+// foo = null;
+// console.log(typeof foo);
+// foo = undefined;
+// console.log(typeof foo);
+// console.log("%c", "background-color: red; padding: 5px;");
+// console.log(1/0);
+// console.log(Infinity);
+// console.log("number"*2); //isNaN()
+// console.log("number"+5*2);
+
+// $age = 13;
+// $message = "sheni asakia: $age";
+// age = 13;
+//  message = "sheni asakia: " + age;
+// alert(message);
+// message = `sheni asakia: ${age}`;
+// console.log(message); 
+let age = 15;
+
+// let checked = false;
+// let isValid = true;
+// let isGreater = 5 > 6;
+
+age = parseInt(prompt("Tqveni asaki"));
+console.log(age);
+age = -age;
+console.log(typeof age);
+console.log(age);
+
+let isOkay = confirm("Kargxar ?");
+console.log(isOkay);
+
+console.log(5/2);
+console.log(parseInt(5/2));
+console.log( 5 % 2 );
+console.log( 8 % 3 );
+
+console.log(2 ** 10);
+
+console.log(25 ** (1/2));
