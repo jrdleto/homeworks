55220b80872ad695d03b83ef23826aa144de2d15
qrsl <david.qarseladze@gmail.com>
2018-08-16 18:28:57 +0400

    sad

---
+++ 15August/realtimeUpdate.js
@@ -0,0 +1,50 @@
+let container = document.getElementById('container');
+    let add = document.getElementById('add');
+function Observer(){
+    this.sourceInput = document.querySelector('#subject');
+
+    this.sourceData = [];
+    this.listeners = [];
+}
+
+Observer.prototype.init = function(){
+    this.listeners = document.querySelectorAll('.listener');
+    this.sourceData.length = this.listeners.length;
+}
+Observer.prototype.update = function(index, func){
+    for( let i = 0 ; i < this.sourceData.length; i++){
+        if( this.sourceData[i] != 'undefined' ){
+            this.listeners[i].innerHTML = this.sourceData[i] || ''
+        }
+    }
+
+    if( func){
+        let currentElement = this.listeners[index]; 
+        func(currentElement)
+    }
+}
+
+Observer.prototype.add = function(index, func){
+    this.sourceData[index] = this.sourceInput.value;
+    this.update(index, func);
+}
+
+const observer = new Observer();
+observer.init();
+
+observer.sourceInput.addEventListener('keyup', () =>{
+   
+    for( let i = 0; i < observer.listeners.length; i++){
+        observer.add(i, function(index){
+            index.style.backgroundColor = 'yellow';
+
+        })
+    }
+})
+
+    add.addEventListener('click', function(){
+        let input = document.createElement('div');
+        input.classList.add('listener');
+        container.appendChild(input);
+        observer.init();
+    })
